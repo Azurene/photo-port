8136135d6017359e05def7b0690110dd806e9249
Azurene <98633770+Azurene@users.noreply.github.com>
2022-07-15 19:22:37 -0700

    add test for Modal component

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 
-function Modal({currentPhoto}) {
-  const {name, category, description, index} = currentPhoto;
+function Modal({ onClose, currentPhoto }) {
+  const {name, category, description, index} = currentPhoto; 
+
+  console.log(currentPhoto);
 
   return(
     <div className='modalBackdrop'>
       <div className="modalContainer">
         <h3 className="modalTitle">{name}</h3>
-        <img src={require(`../../assets/large/${category}/${index}.jpg`).default} alt="current category" />
+        <img src={require(`../../assets/large/${category}/${index}.jpg`).default} alt={name} />
         <p>
           {description}
         </p>
-        <button type="button">
+        <button onClick={onClose} type="button">
           Close this modal
         </button>
       </div>
